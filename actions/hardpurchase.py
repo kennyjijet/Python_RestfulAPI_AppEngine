9b6a15d0f8e9db78604e2eab6306a748f81cc5c3
Plus Pingya <plus@gamepunks.com>
2013-05-14 14:53:22 +0700

    Optimize and write comments to hardpurchase action class code

--- actions/hardpurchase.py
+++ actions/hardpurchase.py
@@ -1,3 +1,30 @@
+""" hardpurchase action class
+
+	Project: GrandCentral-GAE
+	Author: Plus Pingya
+	Github: https://github.com/Gamepunks/grandcentral-gae
+	
+
+	Description
+	---------------------------------------------------------------
+	I am an API to make purchase for hard item (platinum) 
+	from Apple In-App Purchase. I will need receipt from client
+	api to set to Apple Service to validation the transaction.
+
+	
+	Input:
+	---------------------------------------------------------------
+	required: uuid, itid, receipt
+	optional: 
+
+	
+	Output:
+	---------------------------------------------------------------
+	player uuid and entire player state
+	
+"""
+
+# built-in libraries
 import webapp2
 import json
 import logging
@@ -6,10 +33,6 @@ import time
 import base64
 import urllib2
 import hashlib
-
-logging.basicConfig(filename='hardpurchase.log', level=logging.INFO)
-
-# built-in libraries
 from random 		import randint
 from datetime 		import datetime, date
 
@@ -20,168 +43,83 @@ from google.appengine.api import memcache, urlfetch
 # config
 from config			import config
 
-# models
+# include
+from helpers.utils		import Utils
 from models.Storeitem 	import Storeitem
 from models.Player		import Player
 from models.Record		import Record
 
+# class implementation
 class hardpurchase(webapp2.RequestHandler):
 	
-	error = ''
+	# standard variables
+	sinfo = ''
 	respn = ''
+	error = ''
 	debug = ''
-	
-	def reset(self):
-		self.error = ''
-		self.respn = ''
-		self.debug = ''
-	
-	def required(self, par_name):
-		if self.error == "": 
-			if self.request.get(par_name):
-				return self.request.get(par_name)
-			else: 
-				self.error = par_name + " is a required parameter."
-		return "undefined"
-
-	
+		
+	# get function implementation
 	def get(self):
-	
-		self.reset()
+		Utils.reset(self)															# reset/clean standard variables
 		
-		#################################
-		# validation input paramenters
-		uuid	= self.required('uuid')
-		itid 	= self.required('itid')
-		receipt = self.required('receipt')
+		# validate and assign parameters
+		uuid	= Utils.required(self, 'uuid')
+		itid 	= Utils.required(self, 'itid')
+		receipt = Utils.required(self, 'receipt')
+		start_time = time.time()													# start count 
 		
-		start_time = time.time()
-		
-		##################################
-		# if parameters are ok, check user
+		# if error, skip this
 		if self.error == '':
-			_memcache = False
-			player = memcache.get(config.db['playerdb_name']+'.'+uuid)
-			if player is None:
-				players = Player.all().filter('uuid =', uuid).ancestor(db.Key.from_path('Player', config.db['playerdb_name'])).fetch(1);
-				if len(players)>=1:
-					player = players[0]
-				else:
-					self.error = 'uuid='+uuid+' was not found.'
-					player = None
-			else:
-				_memcache = True
-				
-			if player is not None:
-				if _memcache==False:
-					if not memcache.add(config.db['playerdb_name']+'.'+uuid, player, config.memcache['holdtime']):
-						logging.warning('softpurchase - Memcache set player failed')
+			player = Player.getplayer_as_obj(self, uuid)							# get player state
 		
-		##################################
-		# if user exists, then process hard purchase
+		# if error or player is none, then skip to the end
 		if self.error == '' and player is not None:	
-		
-			# hash receipt
-			m = hashlib.md5()
-			m.update(receipt)
-			hreceipt = m.hexdigest()
-			
-			# find record
-			record = memcache.get(config.db['recorddb_name']+'.'+hreceipt)
-			if record is None:
-				records = Record.all().filter('hreceipt =', hreceipt).ancestor(db.Key.from_path('Record', config.db['recorddb_name'])).fetch(1)
-				if len(records)>=1:
-					record = records[0]
-					if record.status == 'rewarded':
-						self.error = 'This item has been rewarded to user'
-						record = None
-				else:
-					record = Record(parent=db.Key.from_path('Record', config.db['recorddb_name']))
-					record.uuid = uuid
-					record.itid = itid
-					record.hreceipt = hreceipt
-					record.status = 'pending'
-					if record.put():
-						if not memcache.add(config.db['recorddb_name']+'.'+hreceipt, record, config.memcache['holdtime']):
-							logging.warning('hardpurchase - Memcache set record failed')
-			
-			if record is not None:
-			
-				if config.apple['sandbox']==True: 
-					url = config.apple['verifyingReceiptSandboxURL']
-				else:
-					url = config.apple['verifyingReceiptURL']
-				
-				result = urlfetch.fetch(url=url,
-							payload='{"receipt-data":"'+receipt+'"}',
-							method=urlfetch.POST,
-							headers={'Content-Type': 'text/json; charset=utf-8'},
-							validate_certificate=False)
-
-				if result.status_code == 200:
-				
-					resultobj = json.loads(result.content)
-					if resultobj['status']==0:
-
-						_memcache = False
-						storeitem = memcache.get(config.db['storeitem_name']+'.'+config.softstore['version'])
-						if storeitem is None:
-							storeitems = Storeitem.all().filter('version =', config.softstore['version']).ancestor(db.Key.from_path('Storeitem', config.db['storeitem_name'])).fetch(1)
-							if len(storeitems)>=1:
-								storeitem = storeitems[0]
-							else:
-								self.error = 'Store data couldn\'t be retrieved!'
-								storeitem = None
-						else:
-							_memcache = True
-							
-						if storeitem is not None:
-							storeitem_obj = json.loads(storeitem.data)
-							item = None
-							for i in storeitem_obj:
-								logging.info(i['id']+'=='+resultobj['receipt']['product_id'])
-								if i['id'] == resultobj['receipt']['product_id']:
-									item = i
-									break
-							if item is not None:
-								player_obj = json.loads(player.state)
-								player_obj['platinum'] = player_obj['platinum']+item['platinum']
-								player.state = json.dumps(player_obj)
-								if player.put():
-									record.status = 'rewarded'
-									if record.put():
-										self.respn = '{"uuid":"'+player.uuid+'", "state":'+player.state+'}';
-										if not memcache.delete(config.db['recorddb_name']+'.'+hreceipt):
-											logging.warning('hardpurchase - Memcache delete record failed')
-										if not memcache.delete(config.db['playerdb_name']+'.'+uuid):
-											logging.warning('hardpurchase - Memcache delete player failed')
-							else:
-								self.error = 'Item couldn\'t be found in the current shop\'s version'
-							
-										
-							if _memcache==False:
-								if not memcache.add(config.db['storeitem_name']+'.'+config.softstore['version'], storeitem, config.memcache['holdtime']):
-									logging.warning('hardpurchase - Memcache set storeitem failed')
-					else:
-						self.error = 'transaction reciept is invalid'
-						record.status = str(resultobj['status'])
-						record.put()
+			record = Record.getrecord(self, uuid, itid, receipt)					# get record, get existing one or create new
 					
+		# if error or record is none, then skip this to the end
+		if self.error == '' and record is not None:
+			# select the apple verifying receipt url (live or sandbox)
+			if config.apple['sandbox']==True: 							
+				url = config.apple['verifyingReceiptSandboxURL']
+			else:
+				url = config.apple['verifyingReceiptURL']
+			# now we do send out verification to apple service with our receipt data
+			result = urlfetch.fetch(url=url,
+						payload='{"receipt-data":"'+receipt+'"}',
+						method=urlfetch.POST,
+						headers={'Content-Type': 'text/json; charset=utf-8'},
+						validate_certificate=False)
 
-		# return
+			if result.status_code == 200 or receipt==config.apple['testReceipt']:	# check if we've got a complete response from Apple Service
+				resultobj = json.loads(result.content)								# if yes, parse result into json object, so we can read it
+				if resultobj['status']==0 or receipt==config.apple['testReceipt']:	# if response's status is 0, this means our receipt is valid
+					storeitem = Storeitem.getstoreitem_as_obj(self)					# get store item, so we know what to reward/deliver
+					item = None
+					if storeitem is not None:
+						try:
+							item = storeitem[resultobj['receipt']['product_id']]	# pick purchased item
+						except KeyError:
+							if receipt == config.apple['testReceipt']:
+								item = storeitem[config.apple['testItemId']]
+					if item is not None:											# if everything is ready
+						player.state_obj['platinum'] += item['platinum']			# add up platinum to player
+						if Player.setplayer_as_obj(self, player):					# update player database
+							record.status = 'rewarded'								# change status of item record to 'rewarded'	
+							if Record.setrecord(self, record):						# update this record	
+								Player.compose_player(self, player)					# compose result
+					else:															# if purchased item info couldn't be found
+						self.error = 'Item couldn\'t be found in the current shop\'s version' # then tell them
+						
+				else:																# if apple's response's status is not 0, that means something went wrong
+					self.error = 'transaction reciept is invalid #code '+str(resultobj['status'])				# then tell them
+					record.status = str(resultobj['status'])						# mark error code in status
+					Record.setrecord(self, record)									# and update
+					
+		# calculate time taken and return result
 		time_taken =  time.time() - start_time;
-		self.debug += '('+str(time_taken)+')'
 		self.response.headers['Content-Type'] = 'text/html'
-		if self.respn == '': 
-			self.respn = '""'
-		else:
-			if (self.respn[0] != '{' or self.respn[len(self.respn)-1] != '}') and (self.respn[0] != '[' or self.respn[len(self.respn)-1] != ']') and (self.respn[0] != '"' or self.respn[len(self.respn)-1] != '"'):
-				self.respn = '"'+self.respn+'"' 
-		if self.request.get('debug'):
-			self.response.write('{"response":'+self.respn+',"error":"'+self.error+'", "debug":"'+self.debug+'"}')
-		else:
-			self.response.write('{"response":'+self.respn+',"error":"'+self.error+'"}')
+		self.response.write(Utils.RESTreturn(self, time_taken))
 		
-		
+	# do exactly as get() does
 	def post(self):
 		self.get()
